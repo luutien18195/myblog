96ea801fe7c0c67ebd651db7838fa7491f5bfbbf
luutien18195 <luutien18195@gmail.com>
2019-02-06 20:09:09 +0700

    add postController

--- src/main/java/com/example/myblog/controller/BlogController.java
+++ src/main/java/com/example/myblog/controller/BlogController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = {"/home","/"})
 public class BlogController {
     @GetMapping
-    String showHomePage(Model model){
+    public String showHomePage(Model model){
         return "home";
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.GET)
+    public String showPostPage(Model model){
+        return "new_post";
     }
 }
