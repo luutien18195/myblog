f30afcd833e0a5b4bd663a559019b27e64d18a14
luutien18195 <luutien18195@gmail.com>
2019-02-13 21:51:42 +0700

    fix type for findAll method in RelationshipRepo

--- src/main/java/com/example/myblog/repository/RelationshipRepository.java
+++ src/main/java/com/example/myblog/repository/RelationshipRepository.java
@@ -6,14 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface RelationshipRepository extends JpaRepository<Relationship, Integer> {
 
     @Query("SELECT r FROM Relationship r WHERE r.followed.id = :id OR r.follower.id = :id ORDER BY r.id DESC")
-    Relationship findRelationshipsByFollowedOrFollowerOrderById(@Param("id") int id);
+    List<Relationship> findRelationshipsByFollowedOrFollowerOrderById(@Param("id") int id);
 
     @Query("SELECT r FROM Relationship r WHERE r.follower.id = :id ORDER BY r.id DESC")
-    Relationship findRelationshipsByFollowerIdOrderById(@Param("id") int id);
+    List<Relationship> findRelationshipsByFollowerIdOrderById(@Param("id") int id);
 
     @Query("SELECT r FROM Relationship r WHERE r.followed.id = :id ORDER BY r.id DESC")
-    Relationship findRelationshipsByFollowedIdOrderById(@Param("id") int id);
+    List<Relationship> findRelationshipsByFollowedIdOrderById(@Param("id") int id);
 }
